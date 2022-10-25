14683a779ef8f9c99b0d3c18d9e0ec4857cca8b2
meganmedia <dave@nectar9.com>
2022-10-25 09:46:58 -0700

    Update strategy to support for V5

--- lib/omniauth/strategies/pinterest.rb
+++ lib/omniauth/strategies/pinterest.rb
@@ -4,9 +4,10 @@ module OmniAuth
   module Strategies
     class Pinterest < OmniAuth::Strategies::OAuth2
       option :client_options, {
+        :ssl => { verify: false },
         :site => 'https://api.pinterest.com',
-        :authorize_url => 'https://api.pinterest.com/oauth/',
-        :token_url => 'https://api.pinterest.com/v1/oauth/token'
+        :authorize_url => 'https://www.pinterest.com/oauth/',
+        :token_url => 'https://api.pinterest.com/v5/oauth/token'
       }
 
       def request_phase
@@ -15,25 +16,28 @@ module OmniAuth
         super
       end
 
-      uid { raw_info['id'] }
-
-      info do
+      credentials do
         {
-          'nickname' => raw_info['url'] && raw_info['url'].split('/').last,
-          'first_name' => raw_info['first_name'],
-          'last_name' => raw_info['last_name'],
-          'name' => [raw_info['first_name'],raw_info['last_name']].compact.join(' ')
+          token: access_token.token,
+          refresh_token: access_token.refresh_token,
+          expires: access_token.expires_in.present?,
+          expires_at: access_token.expires_in.seconds.from_now,
+          refresh_token_expires_at: access_token.params['refresh_token_expires_in'].seconds.from_now
         }
       end
 
-      def raw_info
-        @data ||= access_token.params["user"]
-        unless @data
-          access_token.options[:mode] = :query
-          access_token.options[:param_name] = "access_token"
-          @data ||= access_token.get('/v1/me').parsed['data'] || {}
-        end
-        @data
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
+      def build_access_token
+        Rails.logger.debug "Omniauth build access token"
+        options.token_params.merge!(:headers => {'Authorization' => basic_auth_header })
+        super
+      end
+
+      def basic_auth_header
+        "Basic " + Base64.strict_encode64("#{options[:client_id]}:#{options[:client_secret]}")
       end
     end
   end
