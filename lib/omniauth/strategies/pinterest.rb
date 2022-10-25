bea753a38807a16b8b3a10d33fb172203f9da975
meganmedia <dave@nectar9.com>
2022-10-25 10:32:32 -0700

    Cleanup expires/expires_at

--- lib/omniauth/strategies/pinterest.rb
+++ lib/omniauth/strategies/pinterest.rb
@@ -4,14 +4,13 @@ module OmniAuth
   module Strategies
     class Pinterest < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :ssl => { verify: false },
         :site => 'https://api.pinterest.com',
         :authorize_url => 'https://www.pinterest.com/oauth/',
         :token_url => 'https://api.pinterest.com/v5/oauth/token'
       }
 
       def request_phase
-        options[:scope] ||= 'read_public'
+        options[:scope] ||= 'user_accounts:read'
         options[:response_type] ||= 'code'
         super
       end
@@ -20,9 +19,9 @@ module OmniAuth
         {
           token: access_token.token,
           refresh_token: access_token.refresh_token,
-          expires: access_token.expires_in.present?,
-          expires_at: access_token.expires_in.seconds.from_now,
-          refresh_token_expires_at: access_token.params['refresh_token_expires_in'].seconds.from_now
+          expires: access_token.expires?,
+          expires_at: access_token.expires_at,
+          refresh_token_expires_at: refresh_token_expires_at(access_token.params['refresh_token_expires_in'])
         }
       end
 
@@ -31,13 +30,16 @@ module OmniAuth
       end
 
       def build_access_token
-        Rails.logger.debug "Omniauth build access token"
         options.token_params.merge!(:headers => {'Authorization' => basic_auth_header })
         super
       end
 
       def basic_auth_header
         "Basic " + Base64.strict_encode64("#{options[:client_id]}:#{options[:client_secret]}")
+      end
+
+      def refresh_token_expires_at(expires_in_seconds)
+        Time.now.to_i + expires_in_seconds
       end
     end
   end
